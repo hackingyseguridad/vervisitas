20130b34924b44e765066974be6c63249c88d642
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-04-23 00:35:40 +0200

    Create instalar.sh

---
+++ instalar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 
+# Insalacion como comandos en Linux
+# (c) 2017 hackingyseguridad.com
+#
+#
+chmod 777 vervisitas
+chmod 777 verconexiones
+cp vervisitas /sbin/
+cp verconexiones /sbin/
